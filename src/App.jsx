e7336a40a744c9460b2b73b11e072f1dc90478ce
Sauret <sauret.lucas.ivan@gmail.com>
2025-10-01 00:09:58 -0300

    inicie el proyecto, hice modelo de objeto de personaje en data, añadi las imagenes, hice una pagina de inicio muy básica

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Soul Echoes</h1>
+
+      <p>A lo largo de distintos mundos y tiempos han surgido héroes y villanos que llevaron a cabo grandes hazañas que dejaron su huella en la historia. Las almas de estos personajes hacen eco en los distintos fragmentos de la realidad. Hoy, esos fragmentos se han unido y estos ecos han tomado vida nuevamente. Escucha sus voces: descubre las historias que guardan estas almas mientras te ayudan a continuar con tu objetivo de reunir todos sus relatos.</p>
+    </>
+  )
+}
+
+export default App
