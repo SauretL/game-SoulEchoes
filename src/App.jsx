fb0abd029ecd420941af6e6e01ac3ffa77d7bd54
Sauret <sauret.lucas.ivan@gmail.com>
2025-10-02 21:11:05 -0300

    added more info to the json and made a button to change the DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import './App.css'
+import { useState } from 'react';
 
 function App() {
 
+  //create states for the different page looks
+  const [currentView, setCurrentView] = useState("start")
+
+  // change the current view for the other one
+  const changeView = () => {
+    setCurrentView(currentView === "start" ? "gacha" : "start")
+  }
+
   return (
-    <>
+
+    currentView === "start" ? (
+
+      <>
+        <h1>Soul Echoes</h1>
+
+        <p>A lo largo de distintos mundos y tiempos han surgido héroes y villanos que llevaron a cabo grandes hazañas que dejaron su huella en la historia. Las almas de estos personajes hacen eco en los distintos fragmentos de la realidad. Hoy, esos fragmentos se han unido y estos ecos han tomado vida nuevamente. Escucha sus voces: descubre las historias que guardan estas almas mientras te ayudan a continuar con tu objetivo de reunir todos sus relatos.</p>
+        <button onClick={changeView}>Comienza el juego</button>
+      </>
+    ) : (<>
       <h1>Soul Echoes</h1>
 
-      <p>A lo largo de distintos mundos y tiempos han surgido héroes y villanos que llevaron a cabo grandes hazañas que dejaron su huella en la historia. Las almas de estos personajes hacen eco en los distintos fragmentos de la realidad. Hoy, esos fragmentos se han unido y estos ecos han tomado vida nuevamente. Escucha sus voces: descubre las historias que guardan estas almas mientras te ayudan a continuar con tu objetivo de reunir todos sus relatos.</p>
-    </>
+      <p>¡Empieza el gacha!</p>
+    </>)
   )
 }
 
